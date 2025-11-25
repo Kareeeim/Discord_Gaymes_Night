ca291179fc215019b566c089aa65921a83f91b4f
Kareeeim <karimrafai9@gmail.com>
2025-11-25 23:36:17 +0100

    HEEEEEEEEEELP

---
+++ GiocoDiscordUno.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class GiocoDiscordUno {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int prize = 69;
+
+            System.out.println("Prova a inserire un numero intero: ");
+            int numero = input.nextInt();
+        
+        do {
+            if (numero > prize) {
+                System.out.println("Prova a inserire un numero intero: ");
+                int numero = input.nextInt();
+                System.out.println("Bro, la stai sparando grossa. ");
+                
+
+            } else if (numero < prize) {
+                System.out.println("Vola più in alto bro.");
+            
+            } else {
+                System.out.println("Cazzo fratello, ma hai il disel, complimenti.");
+            
+            } While (numero != prize);
+        
+            scanner.close();
+
+
+
+
+
+        }
+    }
+}
